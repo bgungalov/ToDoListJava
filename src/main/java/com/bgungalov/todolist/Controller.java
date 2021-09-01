a725e4b6fe6747199f41dd0cbac5230838fc8c1e
bgungalov <bgungalov@gmail.com>
2021-09-01 17:16:55 +0300

    textview

--- src/main/java/com/bgungalov/todolist/Controller.java
+++ src/main/java/com/bgungalov/todolist/Controller.java
@@ -1,5 +1,61 @@
 package com.bgungalov.todolist;
 
+import com.bgungalov.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
 
+    private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card",
+                "Buy a 30th birthday card",
+                LocalDate.of(2021, Month.OCTOBER,25));
+        TodoItem item2 = new TodoItem("Doctors appointment",
+                "See doctor at 123 str",
+                LocalDate.of(2021, Month.SEPTEMBER,15));
+        TodoItem item3 = new TodoItem("Finish design",
+                "Buy a 30th birthday card",
+                LocalDate.of(2021, Month.MARCH,25));
+        TodoItem item4 = new TodoItem("Mail birthday card",
+                "Buy a 30th birthday card",
+                LocalDate.of(2021, Month.OCTOBER,25));
+        TodoItem item5 = new TodoItem("Mail birthday card",
+                "Buy a 30th birthday card",
+                LocalDate.of(2021, Month.OCTOBER,25));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        StringBuilder sb = new StringBuilder(item.getDetails());
+        sb.append("\n\n\n\n");
+        sb.append("Due: ");
+        sb.append(item.getDeadline().toString());
+        itemDetailsTextArea.setText(sb.toString());
+    }
 }
---
+++ src/main/java/com/bgungalov/todolist/datamodel/TodoItem.java
@@ -0,0 +1,48 @@
+package com.bgungalov.todolist.datamodel;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Biser Gungalov on 01/09/2021 16:52
+ */
+public class TodoItem {
+
+    private String shortDescription;
+    private String details;
+    private LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return shortDescription;
+    }
+}
